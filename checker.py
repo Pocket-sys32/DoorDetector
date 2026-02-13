87960c6e2742180e395cd5afdaa7d91d62069517
Pocket-sys32 <pcketcs@gmail.com>
2026-02-13 01:56:01 -0800

    OAUTH 2.0 Integration, fully working just needs API credits!

--- checker.py
+++ checker.py
@@ -2,26 +2,31 @@ import serial
 import tweepy
 import time
 import os
-import oauthlib
 from dotenv import load_dotenv
 load_dotenv()
 
-BEARER_KEY = os.environ.get("BEARER_KEY")
-ACCESS_KEY = os.environ.get("ACCESS_KEY")
-ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
-CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
-CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+CLIENTID_SECRET = os.environ.get("CLIENTID_SECRET")
+CLIENTID = os.environ.get("CLIENTID")
 
-
-api = tweepy.Client(bearer_token=BEARER_KEY,
-                    access_token=ACCESS_KEY,
-                    access_token_secret=ACCESS_SECRET,
-                    consumer_key=CONSUMER_KEY,
-                    consumer_secret=CONSUMER_SECRET)
-
-auth = tweepy.OAuth1UserHandler(
-    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+oauth2_user_handler = tweepy.OAuth2UserHandler(
+    client_id=CLIENTID,
+    redirect_uri="http://127.0.0.1",
+    scope=["tweet.read", "tweet.write", "users.read"],
+    # Client Secret is only necessary if using a confidential client
+    client_secret=CLIENTID_SECRET
 )
+
+print(oauth2_user_handler.get_authorization_url())
+
+
+auth = input("Give me the link!")
+access_token = oauth2_user_handler.fetch_token(
+    auth
+)
+client = tweepy.Client(access_token["access_token"])
+
+
+
 
 def readserial(comport, baudrate):
     ser = serial.Serial(comport, baudrate, timeout=0.1)
@@ -35,8 +40,11 @@ while True:
     readserial('COM3', 9600)
     distance = readserial('COM3', 9600)
     if float(distance) > 15.24:
-        api.create_tweet(text='Pockets mouse has moved!')
-        print("tweet has been sent.")
+        response = client.create_tweet(
+        text="The door has opened!",
+        user_auth=False)
+        print(f"https://twitter.com/user/status/{response.data['id']}")
+
     time.sleep(300)
 
 
